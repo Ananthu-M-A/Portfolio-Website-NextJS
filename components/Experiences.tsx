a26a22adb2385a6a9f5a100b6cec2a5a16125da1
Ananthu M A <heartchaserananthu@gmail.com>
2025-03-07 21:16:04 +0530

    Attractive Animation for Experience

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -6,15 +6,16 @@ import { Badge } from "@/components/ui/badge";
 
 const Experiences = () => {
   return (
-    <section className="border-b border-neutral-800 pb-16 px-6 lg:px-16">
+    <section className="border-b border-neutral-800 pb-16 px-4 sm:px-6 lg:px-16">
       <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
         transition={{ duration: 0.5 }}
-        className="my-12 text-center text-4xl font-bold"
+        className="my-12 text-center text-4xl sm:text-5xl font-bold"
       >
         Experiences
       </motion.h2>
+
       <div className="relative border-l border-neutral-700 pl-6">
         {EXPERIENCES.map((experience, index) => (
           <motion.div
@@ -25,16 +26,31 @@ const Experiences = () => {
             className="relative mb-10"
           >
             <div className="absolute -left-[10px] top-3 h-4 w-4 rounded-full bg-cyan-500 shadow-lg"></div>
+
             <Card className="bg-white/10 backdrop-blur-lg shadow-md transition-transform duration-300 hover:scale-105 hover:shadow-cyan-500/50">
               <CardContent className="p-6">
-                <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+                <p className="mb-2 text-sm text-neutral-400">
+                  {experience.year}
+                </p>
+
                 <h6 className="mb-2 text-lg font-semibold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-                  {experience.role} - <span className="text-sm text-purple-100">{experience.type}</span>
+                  {experience.role} -{" "}
+                  <span className="text-sm text-purple-100">
+                    {experience.type}
+                  </span>
                 </h6>
-                <p className="mb-4 text-neutral-400">{experience.description}</p>
+
+                <p className="mb-4 text-neutral-400">
+                  {experience.description}
+                </p>
+
                 <div className="flex flex-wrap gap-2">
                   {experience.technologies.map((tech, index) => (
-                    <Badge key={index} variant="default" className="text-cyan-400 border border-cyan-500">
+                    <Badge
+                      key={index}
+                      variant="default"
+                      className="text-cyan-400 border border-cyan-500 hover:bg-cyan-500 hover:text-white transition-colors"
+                    >
                       {tech}
                     </Badge>
                   ))}
